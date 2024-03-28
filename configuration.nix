5c0eeaaae2b00eaa4ab7737f52c49084cf94f908
leonillont <leonillo@proton.me>
2024-03-28 19:09:31 +0100

    flatpak

--- configuration.nix
+++ configuration.nix
@@ -32,6 +32,9 @@
 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  # Flatpak
+  services.flatpak.enable = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
