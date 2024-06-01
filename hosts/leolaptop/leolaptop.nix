3cd97e05390f1e2daba09fff2b582f27fa4d747a
leonillont <leonillo@proton.me>
2024-06-01 19:56:30 +0200

    algo?

--- hosts/leolaptop/leolaptop.nix
+++ hosts/leolaptop/leolaptop.nix
@@ -52,10 +52,10 @@
   services.tlp = {
     enable = true;
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
-      CPU_SCALING_GOVERNOR_ON_BAT = "ondemand";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "schedutil";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "powersave";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
       CPU_MIN_PERF_ON_AC = 0;
@@ -63,11 +63,13 @@
       CPU_MIN_PERF_ON_BAT = 0;
       CPU_MAX_PERF_ON_BAT = 80;
 
-      #Optional helps save long term battery health
-      START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
-      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
-
     };
+  };
+
+  # Abrir puerto 8000, para la web de tests
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 8000 ];
   };
 
   # Thermald
