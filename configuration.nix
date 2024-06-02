8689722cddbe5c478d879f30f9c409ea972f6fd6
leonillont <leonillo@proton.me>
2024-06-02 22:06:36 +0200

    no se

--- configuration.nix
+++ configuration.nix
@@ -65,10 +65,10 @@
   #networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
   services.resolved = {
     enable = true;
-    dnssec = "true";
+    #dnssec = "true";
     domains = [ "~." ];
-    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
-    dnsovertls = "true";
+    #fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    #dnsovertls = "true";
   };
 
   # Zona horaria
