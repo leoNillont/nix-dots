57d22d8e1e3ed674bd04da520f389ab7d9386269
leonillont <leonillo@proton.me>
2024-07-24 14:49:45 +0200

    añadir regla polkit corectrl

--- hosts/leopc/leopc.nix
+++ hosts/leopc/leopc.nix
@@ -46,6 +46,17 @@
     enable = true;
     gpuOverclock.enable = true;
   };
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if ((action.id == "org.corectrl.helper.init" ||
+         action.id == "org.corectrl.helperkiller.init") &&
+        subject.local == true &&
+        subject.active == true &&
+        subject.isInGroup("wheel")) {
+            return polkit.Result.YES;
+      }
+    });
+  '';
 
   boot.kernelParams = [ "amd_pstate=guided" ];
 
